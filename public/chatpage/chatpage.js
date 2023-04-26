a95ca688fe06855cb7c888fb865d027bf8f59880
AnirudhAtole <anirudhatole@gmail.com>
2023-04-26 12:55:38 +0530

    redesigned the chat page

---
+++ public/chatpage/chatpage.js
@@ -0,0 +1,51 @@
+let textbox = document.getElementById("texts");
+
+textbox.addEventListener("keydown", function(e){
+
+    if(e.key === "Enter"){
+        let current = new Date();
+        let cDate = current.getFullYear() + '-' + (current.getMonth() + 1) + '-' + current.getDate();
+        let cTime = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
+        let dateTime = cDate + ' ' + cTime;
+        if(textbox.value !== ''){
+            createChat("user",textbox.value,dateTime)
+        textbox.value = ""
+        }
+    }
+})
+
+function createChat(sender,chat,time){
+    let chat1 = document.getElementById('chat1')
+    let div = document.createElement('div');
+    if(sender === "user"){
+        div.className = "chat-user"
+        div.innerHTML = `
+        <div class="d-flex justify-content-between">
+          <p class="small mb-1 text-muted">${time}</p>
+          <p class="small mb-1">You</p>
+        </div>
+        <div class="d-flex flex-row justify-content-end mb-4">
+          <div class="p-3 me-3 border" style="border-radius: 15px; background-color: #50C878;">
+            <p class="small mb-0">${chat}</p>
+          </div>
+          <img src="https://img.icons8.com/clouds/1x/mando.png"
+            alt="avatar 1" style="width: 45px; height: 100%;">
+        </div>`
+    }
+    else{
+        div.className = "chat-other"
+        div.innerHTML = `
+        <div class="d-flex justify-content-between">
+          <p class="small mb-1 text-muted">${sender}</p>
+          <p class="small mb-1">${time}</p>
+        </div>
+        <div class="d-flex flex-row justify-content-start mb-4">
+          <img src="https://img.icons8.com/emoji/1x/man-beard.png"
+            alt="avatar 1" style="width: 45px; height: 100%;">
+          <div class="p-3 ms-3" style="border-radius: 15px; background-color: rgba(57, 192, 237,.2);">
+            <p class="small mb-0">${chat}</p>
+          </div>
+        </div>`
+    }
+    chat1.appendChild(div);
+}
